24152a86781c1f394a82889db59376f36ac9d121
ilay zeidman <ilayndeganit@gmail.com>
2015-01-03 20:16:06 +0200

    tabs

---
+++ WebAppWithAngularJS/Scripts/Tabs/tabs.js
@@ -0,0 +1,15 @@
+﻿angular.module('iexcloudApp',['ui.bootstrap']).controller('TabsCtrl', function ($scope, $window) {
+    var vm = this;
+    vm.leftMenu = [
+        {
+            groupName: 'Computers',
+            items: [
+                {name:"a"},{name:"b"}]
+        },
+        {
+            groupName: 'IexServers',
+            items: [
+                { name: "iex1" }, { name: "iex2" }]
+        }
+    ]
+});
