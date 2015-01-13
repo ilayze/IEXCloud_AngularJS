b590167fde6891b3ae716af6a73daed48375cf9d
ilay zeidman <ilayndeganit@gmail.com>
2015-01-13 19:43:26 +0200

    order the folders

---
+++ WebAppWithAngularJS/Scripts/app/mainModule.js
@@ -0,0 +1,31 @@
+﻿angular.module('iexcloudApp', ['ui.bootstrap','ngRoute'])
+    .config(['$routeProvider',
+  function($routeProvider) {
+      $routeProvider.
+        when('/lab', {
+            templateUrl: 'Views/lab.html',
+            controller: 'LabCtrl'
+        }).
+        when('/reports', {
+            templateUrl: 'Views/reports.html',
+            controller: 'ReportsCtrl'
+        }).
+        otherwise({
+            redirectTo: '/lab'
+        });
+  }])
+.controller('LabCtrl', function ($scope, $window) {
+    var vm = this;
+    vm.leftMenu = [
+        {
+            groupName: 'Computers',
+            items: [
+                {name:"a"},{name:"b"}]
+        },
+        {
+            groupName: 'IexServers',
+            items: [
+                { name: "iex1" }, { name: "iex2" }]
+        }
+    ]
+    });
